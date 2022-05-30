479d76cb487ab5d6f82dcab1372bc5271587ec20
ShashiKanth <57086246+shashikanth-t@users.noreply.github.com>
2022-05-30 12:01:36 +0530

    sample.java file added

---
+++ sample.java
@@ -0,0 +1,14 @@
+#this is my first Java Application
+
+import java.lang.*;
+
+class sample
+{
+ public static void main(String[] args)
+ {
+  System.out.println("Welcome To Java Application...");
+  int a=10,b=20,c;
+  c=a+b;
+  System.out.println("Sum of "+a+" and "+b+" is "+c);
+ }
+}
